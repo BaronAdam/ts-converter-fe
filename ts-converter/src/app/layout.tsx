322c5d5a49fdda25b4f71871f5f1b26b677f15ab
Adam Baron <adam.baron452@gmail.com>
2024-01-26 16:37:24 +0100

    Update layout.tsx

--- ts-converter/src/app/layout.tsx
+++ ts-converter/src/app/layout.tsx
@@ -4,10 +4,13 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const appName: string = "Truck Sim Time Converter";
+
 export const metadata: Metadata = {
-  title: "Truck Sim Converter",
+  title: appName,
   description: "Simple app to convert ingame time to real time",
-  manifest: "/manifest.json"
+  manifest: "/manifest.json",
+  applicationName: appName,
 };
 
 export default function RootLayout({
@@ -17,6 +20,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content={appName} />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='theme-color' content='#FFFFFF' />
+
+        <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="384x384" href="/icons/icon-384x384.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-512x512.png" />
+
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="84x384" href="/icons/icon-384x384.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icons/icon-512x512.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
