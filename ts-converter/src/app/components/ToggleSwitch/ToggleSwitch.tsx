4c2c41f14fa8808398290b5fc4ebc42e8b64ba9c
Adam Baron <adam.baron452@gmail.com>
2024-01-26 15:02:51 +0100

    fix layout

--- ts-converter/src/app/components/ToggleSwitch/ToggleSwitch.tsx
+++ ts-converter/src/app/components/ToggleSwitch/ToggleSwitch.tsx
@@ -22,19 +22,20 @@ const ToggleSwitch: FC<ToggleSwitchProps> = ({
 
   return (
     <div
-      className="m-2 flex place-content-between items-center text-xl 
-      text-gray-900"
+      className="m-2 grid grid-cols-6 md:flex md:place-content-between 
+      md:items-center text-xl text-gray-900"
     >
-      <span>{text}</span>
-      <label className="relative inline-block h-6 w-12">
-        <input
-          onChange={handleOnChange}
-          type="checkbox"
-          disabled={disabled}
-          className="peer h-0 w-0 opacity-0"
-        />
-        <span
-          className="slider round absolute bottom-0 end-0 start-0 top-0 
+      <span className="text-wrap col-span-5">{text}</span>
+      <div className="flex justify-end">
+        <label className="relative md:inline-block h-6 w-12 ">
+          <input
+            onChange={handleOnChange}
+            type="checkbox"
+            disabled={disabled}
+            className="peer h-0 w-0 opacity-0"
+          />
+          <span
+            className="w-12 h-6 absolute bottom-0 end-0 start-0 top-0 
           cursor-pointer rounded-3xl bg-gray-300 duration-300 before:absolute
           before:bottom-1 before:left-1 before:h-4 before:w-4 before:rounded-3xl 
           before:bg-white before:duration-300 before:content-['']
@@ -43,8 +44,9 @@ const ToggleSwitch: FC<ToggleSwitchProps> = ({
           peer-focus:border-red-800 peer-focus:shadow peer-focus:shadow-red-800 
           peer-focus:outline-0 peer-disabled:bg-gray-400 
           peer-checked:peer-disabled:bg-red-800"
-        />
-      </label>
+          />
+        </label>
+      </div>
     </div>
   );
 };
